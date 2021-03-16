480d0a704cf17f8ddf695ac6435d2d6f134e1bdc
yang <jane@example.com>
2021-03-16 17:38:43 -0400

    script

---
+++ scripts/00_EDA.R
@@ -0,0 +1,10 @@
+#### Preamble ####
+# Purpose: This script produce the EDA analysis.
+# author: "Yingying Zhou, Yang Wu, Xinyi Xu, Hong Pan"
+# Data: 14 March 2021
+# Contact: yingying.zhou@utoronto.ca; xiny.xu@mail.utoronto.ca; yangg.wu@mail.urotonto.ca
+# License: MIT
+# Pre-requisites: 
+# - None
+
+
